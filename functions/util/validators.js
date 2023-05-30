daf5e153b76915f12977347f1b26ddbdf9943156
Alan <alanl07905@gmail.com>
2023-05-30 13:04:19 -0400

    created validator to validate user

---
+++ functions/util/validators.js
@@ -0,0 +1,60 @@
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+exports.validateLoginData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+const isEmail = (email) => {
+  const emailRegEx =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+};
+const isPhoneNumber = (phoneNumber) => {
+  const phoneRegex = /^(\+0?1\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/;
+  if (phoneNumber.match(phoneRegex)) return true;
+  else return false;
+};
+
+exports.validateSignUpData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(data.phoneNumber)) {
+    errors.phoneNumber = "Must not be empty";
+  } else if (!isPhoneNumber(data.phoneNumber)) {
+    errors.phoneNumber = "Must be a valid phone number";
+  }
+
+  if (isEmpty(data.firstName)) errors.firstName = "Must not be empty";
+  if (isEmpty(data.lastName)) errors.lastName = "Must not be empty";
+  if (isEmpty(data.phoneNumber)) errors.phoneNumber = "Must not be empty";
+  if (isEmpty(data.country)) errors.country = "Must not be empty";
+
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
+  if (data.password.length < 8) errors.password = "Must be at least 8 characters in length"
+  if (data.password !== data.confirmPassword)
+    errors.confirmPassword = "Passwords must be the same";
+  if (isEmpty(data.username)) errors.username = "Must not be empty";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
