7f63c003083c413e5d5564aeeb28c81ef6c5f996
Alan <alanl07905@gmail.com>
2023-05-30 12:47:51 -0400

    added postOneTodo endpoint

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -47,3 +47,32 @@ exports.getOneTodo = async (request, response) => {
     return response.status(500).json({ error: err.code });
   }
 };
+
+exports.postOneTodo = async (request, response) => {
+  try {
+    if (request.body.body.trim() === "") {
+      return response.status(400).json({ body: "Must not be empty" });
+    }
+
+    if (request.body.title.trim() === "") {
+      return response.status(400).json({ title: "Must not be empty" });
+    }
+
+    const newTodoItem = {
+      title: request.body.title,
+      username: request.user.username,
+      body: request.body.body,
+      createdAt: new Date().toISOString()
+    };
+
+    const doc = await db.collection("todos").add(newTodoItem);
+
+    const responseTodoItem = newTodoItem;
+    responseTodoItem.todoId = doc.id
+
+    return response.json(responseTodoItem);
+  } catch (err) {
+    console.error(err);
+    response.status(500).json({ error: "Something went wrong" });
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -5,11 +5,13 @@ const auth = require("./util/auth");
 // Todos Functions
 const {
   getAllTodos,
-  getOneTodo
+  getOneTodo,
+  postOneTodo
 } = require("./APIs/todos");
 
 // Todo API
 app.get("/todos", auth, getAllTodos);
 app.get("/todo/:todoId", auth, getOneTodo);
+app.post("/todo", auth, postOneTodo);
 
 exports.api = functions.https.onRequest(app);
