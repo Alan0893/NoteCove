38e92cde98f7d154e682ab6c8112b1e5d57fbf93
Alan <alanl07905@gmail.com>
2023-05-30 13:04:49 -0400

    added storage to store user profile picture

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,7 +1,11 @@
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const { getStorage } = require("firebase-admin/storage");
+
+const config = require("./config");
 
 const admin = initializeApp(config);
 const db = getFirestore(admin);
+const storage = getStorage(admin);
 
-module.exports = { admin, db };
+module.exports = { admin, db, storage };
