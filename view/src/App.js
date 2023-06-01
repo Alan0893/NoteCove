1c31b3967e3966d49f066569e02faaba638ba6f3
Alan <alanl07905@gmail.com>
2023-06-01 10:19:15 -0400

    added getContrastText

--- view/src/App.js
+++ view/src/App.js
@@ -6,11 +6,20 @@ import Signup from "./pages/signup";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { lighten, darken } from "@mui/material/styles"
-import { light } from "@mui/material/styles/createPalette";
+
+function getContrastText(color) {
+  const hex = color.substring(1);
+  const r = parseInt(hex.substr(0, 2), 16);
+  const g = parseInt(hex.substr(2, 2), 16);
+  const b = parseInt(hex.substr(4, 2), 16);
+  const col = (r*229 + g*587 + b*114) / 1000;
+  return col >= 128 ? "#000" : "#fff";
+}
 
 const mainColor = "#4c8bf5";
 const lightShade = lighten(mainColor, 0.2);
 const darkShade = darken(mainColor, 0.2);
+const contrastText = getContrastText(mainColor);
 
 const theme = createTheme({
   palette: {
@@ -18,7 +27,7 @@ const theme = createTheme({
       light: lightShade,
       main: mainColor,
       dark: darkShade,
-      contrastText: "#fff",
+      contrastText: contrastText,
     },
   },
 });
